a25b1245f36d8621c5b21422a7902f98c08155d5
Wan Jong Kim <wanjongkim1@gmail.com>
2021-04-12 01:01:21 -0400

    Finished Product

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-//import Form from './components/Form'
+import Form from './components/Form'
+import TodoList from './components/ToDoList'
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilters] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  useEffect(() => {
+    filterHandler();
+  }, [todos, filter]);
+
+  const filterHandler = () => {
+    switch(filter) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Wan's To Do List</h1>
+      </header>
+      <Form setFilters={setFilters} inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
+      <TodoList filteredTodos={filteredTodos} todos={todos} setTodos={setTodos} />
     </div>
   );
 }
